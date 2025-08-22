ce4552e3989b0729dc2687abb902b2204e8794d4
yokomizomizuki <yokomizorpc@gmail.com>
2025-08-22 15:49:19 +0900

    レビュー修正_1回目

--- Java/src/SeventhProgram.java
+++ Java/src/SeventhProgram.java
@@ -1,16 +1,28 @@
 package src;
 
+import java.util.Scanner;
+
 public class SeventhProgram {
+	
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int result = add(3,5,6,8);
-		System.out.println(result);
+		// TODO 自動生成されたメソッド・スタブ 
+		Scanner scanner = new Scanner(System.in);
 		
-	}
-	
-	public static int add(int num1, int num2, int... rest) {
-		return num1 + num2;
+		System.out.println("一つ目の引数：");
+		int num1 = scanner.nextInt();
+		
+		System.out.println("二つ目の引数：");
+		int num2 = scanner.nextInt();
+		
+		int result = add(num1,num2);
+		
+		System.out.println("加算の結果：" + result);
+		
+		scanner.close();
 	}
 
 }
